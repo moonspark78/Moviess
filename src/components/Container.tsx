83401938a293f6e34476d298c3a14499b1debaad
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-19 01:42:39 +0200

    :construction:  Work in Progress :construction:

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { CustomComponentProps } from '../interfaces'
+import { mergeClassName } from '../utils'
 
 export const Container = (props: CustomComponentProps) => {
   return (
-    <div className={mergeClassName('', )}></div>
+    <div className={mergeClassName('px-6 py-1.5 max-w-screen-lg mx-auto', )}>{props.children}</div>
   )
 }
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Container } from '../components/Container'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <div>
-      <Container>
-        
+    <div className='bg-header'>
+      <Container className='flex justify-between'>
+        {/* brand & menu */}
+        <div className="flex items-center gap-6">
+          <h1>
+            <Link to={'/'}>MovieList</Link>
+          </h1>
+          <div className="flex items-center gap-1.5">
+            <Link to={'/movies'}></Link>
+          </div>
+        </div>
       </Container>
     </div>
   )
