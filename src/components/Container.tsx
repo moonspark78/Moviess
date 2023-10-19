3041837fb8d4d1022bb29ab73f560c649eacc4b9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-19 14:48:29 +0200

    :construction:  Work in Progress :construction:

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,6 +4,6 @@ import { mergeClassName } from '../utils'
 
 export const Container = (props: CustomComponentProps) => {
   return (
-    <div className={mergeClassName('px-6  max-w-screen-lg mx-auto', )}>{props.children}</div>
+    <div className={mergeClassName('px-6  max-w-screen-lg mx-auto', props.className)}>{props.children}</div>
   )
 }
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Container } from '../components/Container'
 import { Link, useLocation } from 'react-router-dom'
+import {IoIosSearch} from 'react-icons/io'
 
 
 const MENU_CLASS = `
@@ -22,13 +23,21 @@ export const Header = () => {
       <Container className='flex justify-between'>
         {/* brand & menu */}
         <div className="flex items-center gap-6">
+          {/* Brand */}
           <h1 className='text-2xl'>
             <Link to={'/'}>MovieList</Link>
           </h1>
+          {/* Menu */}
           <div className="flex items-center gap-1.5">
             <Link to={'/Movies'}>Movies</Link>
             <Link to={'/Tv'}>Tv</Link>
           </div>
+        </div>
+
+        {/* Search */}
+        <div className="border-b-[1.5px] border-white flex items-center p-1 flex-[0.5]">
+            <input type="text" className='bg-transparent outline-0 flex-1' placeholder='Search...'/>
+            <IoIosSearch size={18}></IoIosSearch>
         </div>
       </Container>
     </div>
