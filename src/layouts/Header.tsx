9e8855a79dbd61b46521b0d548bab475561cdf6f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-19 02:20:31 +0200

    :construction:  Work in Progress :construction:

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from '../components/Container'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+
+
+const MENU_CLASS = `
+  px-3
+  py-1.5
+  hover:bg-primary
+`
+const MENU_CLASS_ACTIVE = `
+  bg-primary
+`
 
 export const Header = () => {
+  const location = useLocation()
+  const [pathname, setPathname] = useState('')
+
+
   return (
     <div className='bg-header'>
       <Container className='flex justify-between'>
         {/* brand & menu */}
         <div className="flex items-center gap-6">
-          <h1>
+          <h1 className='text-2xl'>
             <Link to={'/'}>MovieList</Link>
           </h1>
           <div className="flex items-center gap-1.5">
-            <Link to={'/movies'}></Link>
+            <Link to={'/Movies'}>Movies</Link>
+            <Link to={'/Tv'}>Tv</Link>
           </div>
         </div>
       </Container>
